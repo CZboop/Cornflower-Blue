50995916e27cedcd2b3087a6e7193860aad86e61
C Zeina <czeina@Cs-MacBook-Air.local>
2025-12-10 07:11:21 +0000

    properly handle change of colour from colour picker with state

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,23 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from 'react';
+
+
 
 export default function Home() {
+  const [colour, setColour] = useState("#ffffffff")
+
+  function handleColourPicker(event: React.ChangeEvent<HTMLInputElement>) {
+    setColour(event.target.value)
+  }
+
   return (
     <div className="page-content">
       <h1>Colour Guesser 🎨</h1>
       <div className="colour-picker">
-        <input type="color" id="picker-input" name="picker-input" value="#ffffffff" />
+        <input type="color" id="picker-input" name="picker-input" value={colour} onChange={handleColourPicker} />
       </div>
+      {/* <input type="submit">Submit</input> */}
     </div>
   );
 }
