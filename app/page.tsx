91249f4739d20060cd850aab1d0ef9148bc233e6
Chaam Zeina <zeinachaam@gmail.com>
2025-12-15 20:59:58 +0000

    add picking random colour from json map as colour to guess, show to user

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
 "use client";
 
 import { useState } from 'react';
+import colourMap from './colours/html_colour_map.json';
 
+// console.log(colourMap);
 
 
 export default function Home() {
   const [colour, setColour] = useState("#ffffffff")
+  const [targetColour, setTargetColour] = useState(() => getRandomColour(colourMap))
+
+  function getRandomColour(colourMap: Record<string, string>) {
+    const keys = Object.keys(colourMap);
+    const randomKey = keys[Math.floor(Math.random() * keys.length)];
+    return { name: randomKey, value: colourMap[randomKey] };
+  }
 
   function handleColourPicker(event: React.ChangeEvent<HTMLInputElement>) {
     setColour(event.target.value)
@@ -19,6 +28,9 @@ export default function Home() {
   return (
     <div className="page-content">
       <h1>Colour Guesser 🎨</h1>
+      <h3>Select the colour with this name from the picker:</h3>
+      <h2>{targetColour.name}</h2>
+      {/* TODO: useeffect or something to prevent this changing on load */}
       <form action={formAction}>
         <div className="colour-picker">
           <input type="color" id="picker-input" name="picker-input" value={colour} onChange={handleColourPicker} />
